05fcb11afee0926a8b8a2c8a4c34ab13a82c8c47
Qasim <m.qasim162000@gmail.com>
2022-08-20 02:57:10 +0500

    Navbar almost done, just putting in content left only, working on footer and thinking of ideas

---
+++ src/components/UI/Footer.js
@@ -0,0 +1,111 @@
+import React from "react";
+import changeimg from "./logo.png";
+
+export default function Footer() {
+  return (
+    <div>
+      <footer
+        className="text-center text-white"
+        style={{ backgroundColor: "rgb(33 37 41)" }}
+      >
+        <div className="container p-4">
+          <section className="">
+            <div className="row">
+              <div className="col-lg-2 col-md-12 mb-4 mb-md-0">
+                <div
+                  className="bg-image hover-overlay ripple shadow-1-strong rounded"
+                  data-ripple-color="light"
+                >
+                  <img src={changeimg} alt="footerimage" className="w-100" />
+                  <a href="#!">
+                    <div
+                      className="mask"
+                      style={{ backgroundColor: "rgba(251, 251, 251, 0.2)" }}
+                    ></div>
+                  </a>
+                </div>
+              </div>
+              <div className="col-lg-2 col-md-12 mb-4 mb-md-0">
+                <div
+                  className="bg-image hover-overlay ripple shadow-1-strong rounded"
+                  data-ripple-color="light"
+                >
+                  <img src={changeimg} alt="footerimage" className="w-100" />
+                  <a href="#!">
+                    <div
+                      className="mask"
+                      style={{ backgroundColor: "rgba(251, 251, 251, 0.2)" }}
+                    ></div>
+                  </a>
+                </div>
+              </div>
+              <div className="col-lg-2 col-md-12 mb-4 mb-md-0">
+                <div
+                  className="bg-image hover-overlay ripple shadow-1-strong rounded"
+                  data-ripple-color="light"
+                >
+                  <img src={changeimg} alt="footerimage" className="w-100" />
+                  <a href="#!">
+                    <div
+                      className="mask"
+                      style={{ backgroundColor: "rgba(251, 251, 251, 0.2)" }}
+                    ></div>
+                  </a>
+                </div>
+              </div>
+              <div className="col-lg-2 col-md-12 mb-4 mb-md-0">
+                <div
+                  className="bg-image hover-overlay ripple shadow-1-strong rounded"
+                  data-ripple-color="light"
+                >
+                  <img src={changeimg} alt="footerimage" className="w-100" />
+                  <a href="#!">
+                    <div
+                      className="mask"
+                      style={{ backgroundColor: "rgba(251, 251, 251, 0.2)" }}
+                    ></div>
+                  </a>
+                </div>
+              </div>
+              <div className="col-lg-2 col-md-12 mb-4 mb-md-0">
+                <div
+                  className="bg-image hover-overlay ripple shadow-1-strong rounded"
+                  data-ripple-color="light"
+                >
+                  <img src={changeimg} alt="footerimage" className="w-100" />
+                  <a href="#!">
+                    <div
+                      className="mask"
+                      style={{ backgroundColor: "rgba(251, 251, 251, 0.2)" }}
+                    ></div>
+                  </a>
+                </div>
+              </div>
+              <div className="col-lg-2 col-md-12 mb-4 mb-md-0">
+                <div
+                  className="bg-image hover-overlay ripple shadow-1-strong rounded"
+                  data-ripple-color="light"
+                >
+                  <img src={changeimg} alt="footerimage" className="w-100" />
+                  <a href="#!">
+                    <div
+                      className="mask"
+                      style={{ backgroundColor: "rgba(251, 251, 251, 0.2)" }}
+                    ></div>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </section>
+        </div>
+
+        <div
+          className="text-center p-3"
+          style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
+        >
+          © 2020 Copyright: PUNJAB POLICE, PAKISTAN - ALL RIGHTS RESERVED.
+        </div>
+      </footer>
+    </div>
+  );
+}
