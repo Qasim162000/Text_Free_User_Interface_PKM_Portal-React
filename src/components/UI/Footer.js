e4223b0deed29290f64f981df5943b28706ef655
Qasim <m.qasim162000@gmail.com>
2022-08-20 23:09:04 +0500

    Added footer Card

--- src/components/UI/Footer.js
+++ src/components/UI/Footer.js
@@ -1,6 +1,6 @@
 import React from "react";
-import changeimg from "./logo.png";
 import "../../styles/Footer.css";
+import FooterCard from "./FooterCard";
 
 export default function Footer() {
   return (
@@ -9,97 +9,7 @@ export default function Footer() {
         className="text-center text-white"
         style={{ backgroundColor: "rgb(33 37 41)" }}
       >
-        <div className="container p-4">
-          <section className="">
-            <div className="row">
-              <div className="col-lg-2 col-md-12 mb-4 mb-md-0">
-                <div
-                  className="bg-image hover-overlay ripple shadow-1-strong rounded"
-                  data-ripple-color="light"
-                >
-                  <img src={changeimg} alt="footerimage" className="w-100" />
-                  <a href="#!">
-                    <div
-                      className="mask"
-                      style={{ backgroundColor: "rgba(251, 251, 251, 0.2)" }}
-                    ></div>
-                  </a>
-                </div>
-              </div>
-              <div className="col-lg-2 col-md-12 mb-4 mb-md-0">
-                <div
-                  className="bg-image hover-overlay ripple shadow-1-strong rounded"
-                  data-ripple-color="light"
-                >
-                  <img src={changeimg} alt="footerimage" className="w-100" />
-                  <a href="#!">
-                    <div
-                      className="mask"
-                      style={{ backgroundColor: "rgba(251, 251, 251, 0.2)" }}
-                    ></div>
-                  </a>
-                </div>
-              </div>
-              <div className="col-lg-2 col-md-12 mb-4 mb-md-0">
-                <div
-                  className="bg-image hover-overlay ripple shadow-1-strong rounded"
-                  data-ripple-color="light"
-                >
-                  <img src={changeimg} alt="footerimage" className="w-100" />
-                  <a href="#!">
-                    <div
-                      className="mask"
-                      style={{ backgroundColor: "rgba(251, 251, 251, 0.2)" }}
-                    ></div>
-                  </a>
-                </div>
-              </div>
-              <div className="col-lg-2 col-md-12 mb-4 mb-md-0">
-                <div
-                  className="bg-image hover-overlay ripple shadow-1-strong rounded"
-                  data-ripple-color="light"
-                >
-                  <img src={changeimg} alt="footerimage" className="w-100" />
-                  <a href="#!">
-                    <div
-                      className="mask"
-                      style={{ backgroundColor: "rgba(251, 251, 251, 0.2)" }}
-                    ></div>
-                  </a>
-                </div>
-              </div>
-              <div className="col-lg-2 col-md-12 mb-4 mb-md-0">
-                <div
-                  className="bg-image hover-overlay ripple shadow-1-strong rounded"
-                  data-ripple-color="light"
-                >
-                  <img src={changeimg} alt="footerimage" className="w-100" />
-                  <a href="#!">
-                    <div
-                      className="mask"
-                      style={{ backgroundColor: "rgba(251, 251, 251, 0.2)" }}
-                    ></div>
-                  </a>
-                </div>
-              </div>
-              <div className="col-lg-2 col-md-12 mb-4 mb-md-0">
-                <div
-                  className="bg-image hover-overlay ripple shadow-1-strong rounded"
-                  data-ripple-color="light"
-                >
-                  <img src={changeimg} alt="footerimage" className="w-100" />
-                  <a href="#!">
-                    <div
-                      className="mask"
-                      style={{ backgroundColor: "rgba(251, 251, 251, 0.2)" }}
-                    ></div>
-                  </a>
-                </div>
-              </div>
-            </div>
-          </section>
-        </div>
-
+        <FooterCard />
         <div
           className="text-center p-3"
           style={{ backgroundColor: "rgb(33 37 41)" }}
