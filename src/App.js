ead17e7cb2e2b9e8b092c3e3879d1f2b4dbc0c3b
maverick786123 <fasih682@gmail.com>
2022-08-20 23:09:19 +0500

    Add pages & Edit app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import React from "react";
 import "./App.css";
 import Footer from "./components/UI/Footer";
 import Navbar from "./components/UI/Navbar";
+import { BrowserRouter as Router, Switch, Route } from "react";
+import Home from "./components/pages/Home";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App">>
+      <Router>       
       <Navbar />
+      <Switch>
+        <Route path = "/" exact component={Home} />
+        <Route path = "/Services" exact component= {<Services>}
+      </Switch>
       <Footer />
+      </Router>
     </div>
   );
 }
