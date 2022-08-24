43f0a501a221a87055e37af6bb6e7f4fd11a04fd
Qasim <m.qasim162000@gmail.com>
2022-08-25 03:44:56 +0500

    Fixed ServicesCard.js styling

--- src/components/UI/ServicesCard.js
+++ src/components/UI/ServicesCard.js
@@ -1,92 +1,125 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../../styles/ServicesCard.css";
+
 export default function ServicesCard() {
+  const myStyle = {
+    width: "18rem",
+    padding: 40,
+    overflow: "automatic",
+    margin: "auto",
+    borderRadius: 25,
+  };
+
   return (
     <div>
-      <div className="d-flex justify-content-center p-2">
-        <Link
-        to="services"
-        className="card m-1" style={{ width: "18rem" }}>
-          <img src="https://pkm.punjab.gov.pk/assets/frontend/images/portfolio-2/10.png" className="card-img-top" alt="Cardimg1" />
+      <h1 className="mt-5" style={{ fontSize: 52 }}>
+        Our Services
+      </h1>
+      <div className="d-flex justify-content-center pb-2 pt-2">
+        <Link to="services" className="card m-3" style={myStyle}>
+          <img
+            src="https://pkm.punjab.gov.pk/assets/frontend/images/portfolio-2/10.png"
+            className="card-img-top"
+            alt="Cardimg1"
+          />
           <div className="card-body">
             <h5 className="card-title">Character Certificate</h5>
           </div>
         </Link>
-        <div className="card m-1" style={{ width: "18rem" }}>
-          <img src="https://pkm.punjab.gov.pk/assets/frontend/images/portfolio-2/3.png" className="card-img-top" alt="Cardimg1" />
-          <Link
-          to="services"
-          className="card-body">
+        <Link to="services" className="card m-3" style={myStyle}>
+          <img
+            src="https://pkm.punjab.gov.pk/assets/frontend/images/portfolio-2/3.png"
+            className="card-img-top"
+            alt="Cardimg1"
+          />
+          <div to="services" className="card-body">
             <h5 className="card-title">General Police Verification</h5>
-          </Link>
-        </div>
-        <Link
-        to="services"
-         className="card m-1" style={{ width: "18rem" }}>
-          <img src="https://pkm.punjab.gov.pk/assets/frontend/images/portfolio-2/9.png" className="card-img-top" alt="Cardimg1" />
+          </div>
+        </Link>
+        <Link to="services" className="card m-3" style={myStyle}>
+          <img
+            src="https://pkm.punjab.gov.pk/assets/frontend/images/portfolio-2/9.png"
+            className="card-img-top"
+            alt="Cardimg1"
+          />
           <div className="card-body">
             <h5 className="card-title">Learner Driving License</h5>
           </div>
         </Link>
-        <Link
-        to="services"
-         className="card m-1" style={{ width: "18rem" }}>
-          <img src="https://pkm.punjab.gov.pk/assets/frontend/images/portfolio-2/6.png" className="card-img-top" alt="Cardimg1" />
+        <Link to="services" className="card m-3" style={myStyle}>
+          <img
+            src="https://pkm.punjab.gov.pk/assets/frontend/images/portfolio-2/6.png"
+            className="card-img-top"
+            alt="Cardimg1"
+          />
           <div className="card-body">
             <h5 className="card-title">Employee Registeration</h5>
           </div>
         </Link>
-        <Link
-        to="services"
-         className="card m-1" style={{ width: "18rem" }}>
-          <img src="https://pkm.punjab.gov.pk/assets/frontend/images/portfolio-2/4.png" className="card-img-top" alt="Cardimg1" />
+        <Link to="services" className="card m-3" style={myStyle}>
+          <img
+            src="https://pkm.punjab.gov.pk/assets/frontend/images/portfolio-2/4.png"
+            className="card-img-top"
+            alt="Cardimg1"
+          />
           <div className="card-body">
-            <h5 className="card-title">Copy of FIR</h5> 
+            <h5 className="card-title">Copy of FIR</h5>
           </div>
         </Link>
       </div>
-      <Link
-      to="services"
-       className="d-flex justify-content-center p-2">
-        <div className="card m-1" style={{ width: "18rem" }}>
-          <img src="https://pkm.punjab.gov.pk/assets/frontend/images/portfolio-2/8.png" className="card-img-top" alt="Cardimg1" />
+      <div className="d-flex justify-content-center">
+        <Link to="services" className="card m-3" style={myStyle}>
+          <img
+            src="https://pkm.punjab.gov.pk/assets/frontend/images/portfolio-2/8.png"
+            className="card-img-top"
+            alt="Cardimg1"
+          />
           <div className="card-body">
             <h5 className="card-title">Womens Violence Report</h5>
           </div>
-        </div>
-        <Link
-        to="services"
-         className="card m-1" style={{ width: "18rem" }}>
-          <img src="https://pkm.punjab.gov.pk/assets/frontend/images/portfolio-2/2.png" className="card-img-top" alt="Cardimg1" />
+        </Link>
+        <Link to="services" className="card m-3" style={myStyle}>
+          <img
+            src="https://pkm.punjab.gov.pk/assets/frontend/images/portfolio-2/2.png"
+            className="card-img-top"
+            alt="Cardimg1"
+          />
           <div className="card-body">
             <h5 className="card-title">Crime Report</h5>
           </div>
         </Link>
-        <Link
-        to="services"
-         className="card m-1" style={{ width: "18rem" }}>
-          <img src="https://pkm.punjab.gov.pk/assets/frontend/images/portfolio-2/1.png" className="card-img-top" alt="Cardimg1" />
+        <Link to="services" className="card m-3" style={myStyle}>
+          <img
+            src="https://pkm.punjab.gov.pk/assets/frontend/images/portfolio-2/1.png"
+            className="card-img-top"
+            alt="Cardimg1"
+          />
           <div className="card-body">
             <h5 className="card-title">Loss Report</h5>
           </div>
         </Link>
-        <Link
-        to="services"
-         className="card m-1" style={{ width: "18rem" }}>
-          <img src="https://pkm.punjab.gov.pk/assets/frontend/images/portfolio-2/7.png" className="card-img-top" alt="Cardimg1" />
+        <Link to="services" className="card m-3" style={myStyle}>
+          <img
+            src="https://pkm.punjab.gov.pk/assets/frontend/images/portfolio-2/7.png"
+            className="card-img-top"
+            alt="Cardimg1"
+          />
           <div className="card-body">
             <h5 className="card-title">Tenants Registeration</h5>
           </div>
         </Link>
-        <Link
-        to="services"
-         className="card m-1" style={{ width: "18rem" }}>
-          <img src="https://pkm.punjab.gov.pk/assets/frontend/images/portfolio-2/5.png" className="card-img-top" alt="Cardimg1" />
+        <Link to="services" className="card m-3" style={myStyle}>
+          <img
+            src="https://pkm.punjab.gov.pk/assets/frontend/images/portfolio-2/5.png"
+            className="card-img-top"
+            alt="Cardimg1"
+          />
           <div className="card-body">
             <h5 className="card-title">Vehicle Registeration</h5>
           </div>
         </Link>
-      </Link>
+      </div>
     </div>
   );
 }
