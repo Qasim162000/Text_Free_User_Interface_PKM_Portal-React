df0011ddef588b896c7e6af30c76bb710ff3dac1
Qasim <m.qasim162000@gmail.com>
2022-08-25 01:17:25 +0500

    Fixed the servicesCard styling a bit

--- src/components/UI/ServicesCard.js
+++ src/components/UI/ServicesCard.js
@@ -4,31 +4,31 @@ export default function ServicesCard() {
   return (
     <div>
       <div className="d-flex justify-content-center p-2">
-        <div className="card" style={{ width: "18rem" }}>
+        <div className="card m-1" style={{ width: "18rem" }}>
           <img src="..." className="card-img-top" alt="Cardimg1" />
           <div className="card-body">
             <h5 className="card-title">Card title</h5>
           </div>
         </div>
-        <div className="card" style={{ width: "18rem" }}>
+        <div className="card m-1" style={{ width: "18rem" }}>
           <img src="..." className="card-img-top" alt="Cardimg1" />
           <div className="card-body">
             <h5 className="card-title">Card title</h5>
           </div>
         </div>
-        <div className="card" style={{ width: "18rem" }}>
+        <div className="card m-1" style={{ width: "18rem" }}>
           <img src="..." className="card-img-top" alt="Cardimg1" />
           <div className="card-body">
             <h5 className="card-title">Card title</h5>
           </div>
         </div>
-        <div className="card" style={{ width: "18rem" }}>
+        <div className="card m-1" style={{ width: "18rem" }}>
           <img src="..." className="card-img-top" alt="Cardimg1" />
           <div className="card-body">
             <h5 className="card-title">Card title</h5>
           </div>
         </div>
-        <div className="card" style={{ width: "18rem" }}>
+        <div className="card m-1" style={{ width: "18rem" }}>
           <img src="..." className="card-img-top" alt="Cardimg1" />
           <div className="card-body">
             <h5 className="card-title">Card title</h5>
@@ -36,31 +36,31 @@ export default function ServicesCard() {
         </div>
       </div>
       <div className="d-flex justify-content-center p-2">
-        <div className="card" style={{ width: "18rem" }}>
+        <div className="card m-1" style={{ width: "18rem" }}>
           <img src="..." className="card-img-top" alt="Cardimg1" />
           <div className="card-body">
             <h5 className="card-title">Card title</h5>
           </div>
         </div>
-        <div className="card" style={{ width: "18rem" }}>
+        <div className="card m-1" style={{ width: "18rem" }}>
           <img src="..." className="card-img-top" alt="Cardimg1" />
           <div className="card-body">
             <h5 className="card-title">Card title</h5>
           </div>
         </div>
-        <div className="card" style={{ width: "18rem" }}>
+        <div className="card m-1" style={{ width: "18rem" }}>
           <img src="..." className="card-img-top" alt="Cardimg1" />
           <div className="card-body">
             <h5 className="card-title">Card title</h5>
           </div>
         </div>
-        <div className="card" style={{ width: "18rem" }}>
+        <div className="card m-1" style={{ width: "18rem" }}>
           <img src="..." className="card-img-top" alt="Cardimg1" />
           <div className="card-body">
             <h5 className="card-title">Card title</h5>
           </div>
         </div>
-        <div className="card" style={{ width: "18rem" }}>
+        <div className="card m-1" style={{ width: "18rem" }}>
           <img src="..." className="card-img-top" alt="Cardimg1" />
           <div className="card-body">
             <h5 className="card-title">Card title</h5>
