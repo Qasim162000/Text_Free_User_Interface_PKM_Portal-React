5c0e03f310304c95f13c691c7238e3b178f3bf77
Qasim <m.qasim162000@gmail.com>
2022-08-25 01:46:08 +0500

    fixed url issue

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Home from "./components/pages/Home";
 import Centers from "./components/pages/Centers";
 import Services from "./components/pages/Services";
 import SignIn from "./components/pages/SignIn";
-import SignOut from "./components/pages/SignOut";
+import SignUp from "./components/pages/SignUp";
 import Track from "./components/pages/Track";
 import Verify from "./components/pages/Verify";
 import Global from "./components/pages/Global";
@@ -19,13 +19,13 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="Services" element={<Services />} />
-          <Route path="Centers" element={<Centers />} />
-          <Route path="Global" element={<Global />} />
-          <Route path="SignIn" element={<SignIn />} />
-          <Route path="SignOut" element={<SignOut />} />
-          <Route path="Track" element={<Track />} />
-          <Route path="Verify" element={<Verify />} />
+          <Route path="services" element={<Services />} />
+          <Route path="centers" element={<Centers />} />
+          <Route path="global" element={<Global />} />
+          <Route path="sign-in" element={<SignIn />} />
+          <Route path="sign-up" element={<SignUp />} />
+          <Route path="track" element={<Track />} />
+          <Route path="verify" element={<Verify />} />
         </Routes>
         <Footer />
       </BrowserRouter>
--- src/components/pages/SignIn.js
+++ src/components/pages/SignIn.js
@@ -1,7 +1,172 @@
-import React from 'react'
+import React, { useState } from "react";
+import validator from "validator";
 
 export default function SignIn() {
+  const [message, setMessage] = useState("");
+  const [error, setError] = useState(null);
+
+  function isValidEmail(email) {
+    return /\S+@\S+\.\S+/.test(email);
+  }
+
+  const handleChange = (event) => {
+    if (!isValidEmail(event.target.value)) {
+      setError("Email is invalid");
+    } else {
+      setError(null);
+    }
+
+    setMessage(event.target.value);
+  };
+
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const validate = (value) => {
+    if (
+      validator.isStrongPassword(value, {
+        minLength: 8,
+        minLowercase: 1,
+        minUppercase: 1,
+        minNumbers: 1,
+        minSymbols: 1,
+      })
+    ) {
+      setErrorMessage("Is Strong Password");
+    } else {
+      setErrorMessage("Is Not Strong Password");
+    }
+  };
+
   return (
-    <div>SignIn</div>
-  )
+    <>
+      <section
+        className="vh-100"
+        style={{ "background-color": "eee", margin: "40px 0px" }}
+      >
+        <div className="container h-100">
+          <div className="row d-flex justify-content-center align-items-center h-100">
+            <div className="col-lg-12 col-xl-11">
+              <div
+                className="card text-black"
+                style={{ "border-radius": "25px" }}
+              >
+                <div className="card-body p-md-5">
+                  <div className="row justify-content-center">
+                    <div className="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
+                      <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">
+                        Sign in
+                      </p>
+
+                      <form className="mx-1 mx-md-4">
+                        <div className="d-flex flex-row align-items-center mb-4">
+                          <i className="fas fa-envelope fa-lg me-3 pb-5 fa-fw"></i>
+                          <div className="form-outline flex-fill mb-0">
+                            <label
+                              className="form-label pb-2"
+                              htmlFor="form3Example3c"
+                            >
+                              Your Email
+                            </label>
+
+                            <div>
+                              <input
+                                className="form-control"
+                                id="message"
+                                name="message"
+                                value={message}
+                                onChange={handleChange}
+                              />
+
+                              {error && <p style={{ color: "red" }}>{error}</p>}
+                            </div>
+                          </div>
+                        </div>
+
+                        <div className="d-flex flex-row align-items-center mb-4">
+                          <i className="fas fa-lock fa-lg me-3 pb-5 mb-2 fa-fw"></i>
+                          <div className="form-outline flex-fill mb-0">
+                            <label
+                              className="form-label pb-2"
+                              htmlFor="form3Example4c"
+                            >
+                              Password
+                            </label>
+                            {/* <input
+                              type="password"
+                              id="form3Example4c"
+                              className="form-control"
+                            /> */}
+                            <div>
+                              <pre>
+                                <input
+                                  type="password"
+                                  className="form-control"
+                                  onChange={(e) => validate(e.target.value)}
+                                ></input>{" "}
+                                <br />
+                                {errorMessage === "" ? null : (
+                                  <span
+                                    style={{
+                                      fontWeight: "bold",
+                                      color: "red",
+                                    }}
+                                  >
+                                    {errorMessage}
+                                  </span>
+                                )}
+                              </pre>
+                            </div>
+                          </div>
+                        </div>
+
+                        <div className="form-check d-flex justify-content-center mb-5">
+                          <input
+                            className="form-check-input me-2"
+                            type="checkbox"
+                            value=""
+                            id="form2Example3c"
+                          />
+                          <label
+                            className="form-check-label"
+                            htmlFor="form2Example3"
+                          >
+                            Remember me
+                          </label>
+                        </div>
+
+                        <div className="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
+                          <button
+                            type="button"
+                            className="btn btn-primary btn-lg"
+                          >
+                            Sign In
+                          </button>
+                        </div>
+
+                        <div className="form-check d-flex justify-content-center mb-5">
+                          <label
+                            className="form-check-label"
+                            htmlFor="form2Example3"
+                          >
+                            Not a member? <a href="/sign-up">Register</a>
+                          </label>
+                        </div>
+                      </form>
+                    </div>
+                    <div className="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
+                      <img
+                        src="https://img.freepik.com/premium-vector/login-form-template_123447-475.jpg?w=2000"
+                        className="img-fluid"
+                        alt="Login-page"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
