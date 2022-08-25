be71c584a736e3fcde1dbb229fa2689403069adf
Qasim <m.qasim162000@gmail.com>
2022-08-25 19:12:38 +0500

    The services and centers blueprint has been set up, just need to input content now

--- src/components/pages/Centers.js
+++ src/components/pages/Centers.js
@@ -1,7 +1,31 @@
-import React from 'react'
+import Col from "react-bootstrap/Col";
+import ListGroup from "react-bootstrap/ListGroup";
+import Row from "react-bootstrap/Row";
+import Tab from "react-bootstrap/Tab";
 
-export default function Centers() {
+function Centers() {
   return (
-    <div>Centers</div>
-  )
+    <Tab.Container id="list-group-tabs-example" defaultActiveKey="#link1">
+      <Row>
+        <Col sm={4}>
+          <ListGroup>
+            <ListGroup.Item action href="#link1">
+              Link 1
+            </ListGroup.Item>
+            <ListGroup.Item action href="#link2">
+              Link 2
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col sm={8}>
+          <Tab.Content>
+            <Tab.Pane eventKey="#link1">asdnaskdn</Tab.Pane>
+            <Tab.Pane eventKey="#link2">Meow</Tab.Pane>
+          </Tab.Content>
+        </Col>
+      </Row>
+    </Tab.Container>
+  );
 }
+
+export default Centers;
