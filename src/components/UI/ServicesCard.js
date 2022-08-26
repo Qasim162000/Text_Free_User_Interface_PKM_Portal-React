0139711f658bdebdeca5287ba6476eb3825e0c6b
maverickphp <fasih682@gmail.com>
2022-08-26 17:06:20 +0500

    Edited services

--- src/components/UI/ServicesCard.js
+++ src/components/UI/ServicesCard.js
@@ -18,7 +18,7 @@ export default function ServicesCard() {
         Our Services
       </h1>
       <div className="d-flex justify-content-center pb-2 pt-2">
-        <Link to="services" className="card m-1">
+        <Link to="services#id=a" className="card m-1">
           <img
             style={myStyle}
             src="https://pkm.punjab.gov.pk/assets/frontend/images/portfolio-2/10.png"
@@ -29,18 +29,18 @@ export default function ServicesCard() {
             <h5 className="card-title">Character Certificate</h5>
           </div>
         </Link>
-        <Link to="services" className="card m-1">
+        <Link to="services#id=b" className="card m-1">
           <img
             style={myStyle}
             src="https://pkm.punjab.gov.pk/assets/frontend/images/portfolio-2/3.png"
             className="card-img-top  img-thumbnail"
             alt="Cardimg1"
           />
-          <div to="services" className="card-body">
+          <div className="card-body">
             <h5 className="card-title">General Police Verification</h5>
           </div>
         </Link>
-        <Link to="services" className="card m-1">
+        <Link to="services#id=c" className="card m-1">
           <img
             style={myStyle}
             src="https://pkm.punjab.gov.pk/assets/frontend/images/portfolio-2/9.png"
@@ -51,7 +51,7 @@ export default function ServicesCard() {
             <h5 className="card-title">Learner Driving License</h5>
           </div>
         </Link>
-        <Link to="services" className="card m-1">
+        <Link to="services#id=h" className="card m-1">
           <img
             style={myStyle}
             src="https://pkm.punjab.gov.pk/assets/frontend/images/portfolio-2/6.png"
@@ -62,7 +62,7 @@ export default function ServicesCard() {
             <h5 className="card-title">Employee Registeration</h5>
           </div>
         </Link>
-        <Link to="services" className="card m-1">
+        <Link to="services#id=n" className="card m-1">
           <img
             style={myStyle}
             src="https://pkm.punjab.gov.pk/assets/frontend/images/portfolio-2/4.png"
@@ -75,7 +75,7 @@ export default function ServicesCard() {
         </Link>
       </div>
       <div className="d-flex justify-content-center">
-        <Link to="services" className="card m-1">
+        <Link to="services#id=m" className="card m-1">
           <img
             style={myStyle}
             src="https://pkm.punjab.gov.pk/assets/frontend/images/portfolio-2/8.png"
@@ -86,7 +86,7 @@ export default function ServicesCard() {
             <h5 className="card-title">Womens Violence Report</h5>
           </div>
         </Link>
-        <Link to="services" className="card m-1">
+        <Link to="services#id=l" className="card m-1">
           <img
             style={myStyle}
             src="https://pkm.punjab.gov.pk/assets/frontend/images/portfolio-2/2.png"
@@ -97,7 +97,7 @@ export default function ServicesCard() {
             <h5 className="card-title">Crime Report</h5>
           </div>
         </Link>
-        <Link to="services" className="card m-1">
+        <Link to="services#id=k" className="card m-1">
           <img
             style={myStyle}
             src="https://pkm.punjab.gov.pk/assets/frontend/images/portfolio-2/1.png"
@@ -108,7 +108,7 @@ export default function ServicesCard() {
             <h5 className="card-title">Loss Report</h5>
           </div>
         </Link>
-        <Link to="services" className="card m-1">
+        <Link to="services#id=i" className="card m-1">
           <img
             style={myStyle}
             src="https://pkm.punjab.gov.pk/assets/frontend/images/portfolio-2/7.png"
@@ -119,7 +119,7 @@ export default function ServicesCard() {
             <h5 className="card-title">Tenants Registeration</h5>
           </div>
         </Link>
-        <Link to="services" className="card m-1">
+        <Link to="services#id=j" className="card m-1">
           <img
             style={myStyle}
             src="https://pkm.punjab.gov.pk/assets/frontend/images/portfolio-2/5.png"
