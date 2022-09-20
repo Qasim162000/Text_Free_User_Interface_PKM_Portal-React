dba462e7d4378a7a2b7d8086da190f0db996fcdc
Qasim <m.qasim162000@gmail.com>
2022-09-20 08:00:25 +0500

    At backend: Login user authentication functionality added

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -6,7 +6,7 @@ const User = require("../models/User");
 var jwt = require("jsonwebtoken");
 
 const JWT_SECRET = "Pkmportal@punjabpolice";
-//Route 1: Create a user using: POST "/api/auth/createuser". Doesn't reuqire auth
+//Route 1: Create a user using: POST "/api/auth/createuser". No Login required
 router.post(
   "/createuser",
   [
@@ -44,14 +44,56 @@ router.post(
           id: user.id,
         },
       };
-
       const authToken = jwt.sign(data, JWT_SECRET);
       res.json({ authToken });
     } catch (error) {
       console.log(error.message);
       res.status(500).send("Some error occured");
     }
-  }
+  },
+
+  //Route 2: Authenticate a user using: POST "/api/auth/login". No Login required
+  router.post(
+    "/login",
+    [
+      // Validation Check
+      body("email", "Enter a valid email").isEmail(),
+      body("password", "Password cannot be blank").exists(),
+    ],
+    async (req, res) => {
+      //Check for errors
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+      const { email, password } = req.body;
+      try {
+        let user = await User.findOne({ email });
+        if (!user) {
+          return res
+            .status(400)
+            .json({ error: "Please try to login with correct credentials" });
+        }
+        const passwordCompare = await bcrypt.compare(password, user.password);
+        if (!passwordCompare) {
+          return res
+            .status(400)
+            .json({ error: "Please try to login with correct credentials" });
+        }
+
+        const data = {
+          user: {
+            id: user.id,
+          },
+        };
+        const authToken = jwt.sign(data, JWT_SECRET);
+        res.json({ authToken });
+      } catch (error) {
+        console.log(error.message);
+        res.status(500).send("Internal Server Error");
+      }
+    }
+  )
 );
 
 module.exports = router;
