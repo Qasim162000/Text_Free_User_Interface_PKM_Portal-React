b2bec8ce07688c1448e38b4a1dc811bef5dc7b76
Qasim <m.qasim162000@gmail.com>
2022-10-09 08:36:53 +0500

    Backend complete (for now)

--- backend/routes/form.js
+++ backend/routes/form.js
@@ -51,4 +51,59 @@ router.post(
   }
 );
 
+//Route 3: Update an existing Form using: PUT "/api/form/updateform/:id". Login required
+router.put("/updateform/:id", fetchuser, async (req, res) => {
+  const { subject, message } = req.body;
+  try {
+    //Create a newForm Object
+    const newForm = {};
+    if (subject) {
+      newForm.subject = subject;
+    }
+    if (message) {
+      newForm.message = message;
+    }
+
+    //Find the form to be udpated and update it
+    let form = await Form.findById(req.params.id);
+    if (!form) {
+      return res.status(404).send("Not Found");
+    }
+
+    if (form.user.toString() !== req.user.id) {
+      return res.status(401).send("Not Allowed");
+    }
+
+    form = await Form.findByIdAndUpdate(
+      req.params.id,
+      { $set: newForm },
+      { new: true }
+    );
+    res.json(form);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+//Route 4: Delete an existing Form using: DELETE "/api/form/deleteform/:id". Login required
+router.delete("/deleteform/:id", fetchuser, async (req, res) => {
+  try {
+    //Find the form to be deleted and delete it
+    let form = await Form.findById(req.params.id);
+    if (!form) {
+      return res.status(404).send("Not Found");
+    }
+    //Allow deletion only if user owns this form
+    if (form.user.toString() !== req.user.id) {
+      return res.status(401).send("Not Allowed");
+    }
+
+    form = await Form.findByIdAndDelete(req.params.id);
+    res.json({ Success: "Form has been deleted", form: form });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send("Internal Server Error");
+  }
+});
 module.exports = router;
