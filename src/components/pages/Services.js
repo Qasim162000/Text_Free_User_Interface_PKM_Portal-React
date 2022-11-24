6530699ceb44fe5e4f966ae1c3df17d9aaf2f032
Muhammad Qasim <m.qasim162000@gmail.com>
2022-11-24 13:24:31 +0500

    fixed the outlet of services  + Added a form  + visual info section properly

--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -2,12 +2,7 @@ import Col from "react-bootstrap/Col";
 import ListGroup from "react-bootstrap/ListGroup";
 import Row from "react-bootstrap/Row";
 import Tab from "react-bootstrap/Tab";
-import ReactAudioPlayer from "react-audio-player";
-import test from "./test.mp3";
-import testimg from "../UI/SliderImages/bg1.jpg";
-import ServicesSlider from "../UI/ServicesSlider";
-import formServices from "../UI/OtherImages/formServices.png";
-import slideshowServices from "../UI/OtherImages/slideshowServices.png";
+import ServicesAccordion from "../UI/ServicesAccordion";
 
 export default function Services(props) {
   document.title = "PKM Punjab - Our Services";
@@ -72,122 +67,46 @@ export default function Services(props) {
         <Col sm={9}>
           <Tab.Content className="col-11" style={myStyle}>
             <Tab.Pane eventKey="#id=a">
-              <ServicesSlider img1={testimg} img2={testimg} img3={testimg} />
-              <ReactAudioPlayer className="mt-4" src={test} controls />
+              <ServicesAccordion />
             </Tab.Pane>
             <Tab.Pane eventKey="#id=b">
-              <ServicesSlider img1={testimg} img2={testimg} img3={testimg} />
-              <ReactAudioPlayer className="mt-4" src={test} controls />
+              <ServicesAccordion />
             </Tab.Pane>
             <Tab.Pane {...props.changeProgress(70)} eventKey="#id=c">
-              <ServicesSlider img1={testimg} img2={testimg} img3={testimg} />
-              <ReactAudioPlayer className="mt-4" src={test} controls />
+              <ServicesAccordion />
             </Tab.Pane>
             <Tab.Pane eventKey="#id=d">
-              <ServicesSlider img1={testimg} img2={testimg} img3={testimg} />
-              <ReactAudioPlayer className="mt-4" src={test} controls />
+              <ServicesAccordion />
             </Tab.Pane>
             <Tab.Pane eventKey="#id=e">
-              <ServicesSlider img1={testimg} img2={testimg} img3={testimg} />
-              <ReactAudioPlayer className="mt-4" src={test} controls />
+              <ServicesAccordion />
             </Tab.Pane>
             <Tab.Pane eventKey="#id=f">
-              <ServicesSlider img1={testimg} img2={testimg} img3={testimg} />
-              <ReactAudioPlayer className="mt-4" src={test} controls />
+              <ServicesAccordion />
             </Tab.Pane>
             <Tab.Pane eventKey="#id=g">
-              <ServicesSlider img1={testimg} img2={testimg} img3={testimg} />
-              <ReactAudioPlayer className="mt-4" src={test} controls />
+              <ServicesAccordion />
             </Tab.Pane>
             <Tab.Pane eventKey="#id=h">
-              <ServicesSlider img1={testimg} img2={testimg} img3={testimg} />
-              <ReactAudioPlayer className="mt-4" src={test} controls />
+              <ServicesAccordion />
             </Tab.Pane>
             <Tab.Pane eventKey="#id=i">
-              <ServicesSlider img1={testimg} img2={testimg} img3={testimg} />
-              <ReactAudioPlayer className="mt-4" src={test} controls />
+              <ServicesAccordion />
             </Tab.Pane>
             <Tab.Pane eventKey="#id=j">
-              <ServicesSlider img1={testimg} img2={testimg} img3={testimg} />
-              <ReactAudioPlayer className="mt-4" src={test} controls />
+              <ServicesAccordion />
             </Tab.Pane>
             <Tab.Pane eventKey="#id=k">
-              <ServicesSlider img1={testimg} img2={testimg} img3={testimg} />
-              <ReactAudioPlayer className="mt-4" src={test} controls />
+              <ServicesAccordion />
             </Tab.Pane>
             <Tab.Pane eventKey="#id=l">
-              <ServicesSlider img1={testimg} img2={testimg} img3={testimg} />
-              <ReactAudioPlayer className="mt-4" src={test} controls />
+              <ServicesAccordion />
             </Tab.Pane>
             <Tab.Pane eventKey="#id=m">
-              <ServicesSlider img1={testimg} img2={testimg} img3={testimg} />
-              <ReactAudioPlayer className="mt-4" src={test} controls />
+              <ServicesAccordion />
             </Tab.Pane>
             <Tab.Pane eventKey="#id=n">
-              <div className="accordion" id="accordionExample">
-                <div className="accordion-item">
-                  <h2 className="accordion-header" id="headingTwo">
-                    <button
-                      className="accordion-button collapsed"
-                      type="button"
-                      data-bs-toggle="collapse"
-                      data-bs-target="#collapseTwo"
-                      aria-expanded="false"
-                      aria-controls="collapseTwo"
-                    >
-                      <img
-                        src={slideshowServices}
-                        width="5%"
-                        alt="Slideshow logo"
-                      />
-                    </button>
-                  </h2>
-                  <div
-                    id="collapseTwo"
-                    className="accordion-collapse collapse show"
-                    aria-labelledby="headingTwo"
-                    data-bs-parent="#accordionExample"
-                  >
-                    <div className="accordion-body">
-                      <ServicesSlider
-                        img1={testimg}
-                        img2={testimg}
-                        img3={testimg}
-                      />
-                      <ReactAudioPlayer
-                        {...props.changeProgress(100)}
-                        className="mt-4"
-                        src={test}
-                        controls
-                      />
-                    </div>
-                  </div>
-                </div>
-                <div className="accordion-item">
-                  <h2 className="accordion-header" id="headingOne">
-                    <button
-                      className="accordion-button"
-                      type="button"
-                      data-bs-toggle="collapse"
-                      data-bs-target="#collapseOne"
-                      aria-expanded="true"
-                      aria-controls="collapseOne"
-                    >
-                      <img src={formServices} width="5%" alt="Form logo" />
-                    </button>
-                  </h2>
-                  <div
-                    id="collapseOne"
-                    className="accordion-collapse collapse"
-                    aria-labelledby="headingOne"
-                    data-bs-parent="#accordionExample"
-                  >
-                    <div className="accordion-body">
-                      <strong>This is the first item's accordion body.</strong>
-                    </div>
-                  </div>
-                </div>
-              </div>
+              <ServicesAccordion {...props.changeProgress(100)} />
             </Tab.Pane>
           </Tab.Content>
         </Col>
